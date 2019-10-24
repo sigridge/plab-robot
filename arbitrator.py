6be6d8a62fc32e59920019fad94055143031c136
mathildehaugum <mathilhh@stud.ntnu.no>
2019-10-24 14:24:23 +0200

    changes to bbcon and implementation of arbitrator

--- arbitrator.py
+++ arbitrator.py
@@ -2,7 +2,52 @@
 
 
 class Arbitrator:
-    def __init__(self, this_bbcon):
+    def __init__(self, this_bbcon, bbcon_det):
         self.bbcon = this_bbcon
+        self.det_choose_action = bbcon_det  # boolean used to let bbcon decide
+        # if it wants to use deterministic choose_action
+
+    def choose_action(self):
+        """Calls deterministic og stochastic choose_action depending on value of det_choose_action"""
+        if self.det_choose_action:
+            self.choose_action_deterministic()
+        else:
+            self.choose_action_stochastic()
+
+    def choose_action_deterministic(self):
+        """Access all active behaviors from bbcon and chooses the behavior
+        with largest weight. Returns motor recommendations of this behavior"""
+        active_behaviors = self.bbcon.active_behaviors
+        winner_behavior = active_behaviors[0]
+        max_weight = winner_behavior.get_weight()
+        for this_behavior in active_behaviors:
+            if this_behavior.get_weight() > max_weight:  # if there is a behavior with larger weight
+                winner_behavior = this_behavior
+                max_weight = this_behavior.get_weight()
+        if winner_behavior.get_halt_request()
 
 
+        return winner_behavior.get_motor_recs()  # return motor recommendation of behavior with largest weight
+
+    def choose_action_stochastic(self):
+        """Access all active behaviors from bbcon and chooses the behavior
+        with largest weight. Returns motor recommendations of this behavior"""
+        num = 0
+        dict_behaviors = {}  # ex: {B1: [0, 0.8], B2: [0.8, 1.3], B3: [1.3, 2.0]}
+        active_behaviors = self.bbcon.active_behaviors
+
+        # Fill dictionary with behavior-objects and their weight range
+        for this_behavior in active_behaviors:
+            weight = this_behavior.get_weight()
+            dict_behaviors[this_behavior] = [num, num + weight]
+            num += weight
+
+        r = random.uniform(0, num)
+        winner_behavior = None
+
+        # Iterate through dictionary and sets winner_behavior
+        # equal the one with weight range that covers r
+        for key, value in dict_behaviors.items():
+            if value[1] < r:  # makes sure r matches with largest possible weight range
+                winner_behavior = key
+        return winner_behavior.get_motor_recs()  # return motor recommendation of behavior with weight that covers r
