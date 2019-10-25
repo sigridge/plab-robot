39528900701f6b5635e199acd79edc2fc872ff88
VildeStabell <vilde.voss.stabell@gmail.com>
2019-10-25 10:58:32 +0200

    Må visst bruke komma, ikke pluss

--- bbcon.py
+++ bbcon.py
@@ -32,7 +32,7 @@ class Bbcon:
         # Add all sensobs to BBCON:
         i = 0
         for this_behavior in self.behaviors:
-            print("behaviour " + i)
+            print("behaviour ", i)
             if (this_behavior.get_sensob() not in self.sensobs) and (this_behavior.get_sensob() is not None):  # sensobs added once
                 self.add_sensob(this_behavior.get_sensob())
                 print("Added sensob")
