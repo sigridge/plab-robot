2a8dcac2b0302da19058a29a3419de5a55b50277
VildeStabell <vilde.voss.stabell@gmail.com>
2019-10-25 13:19:15 +0200

    Excessive debugging in sensob

--- sensob.py
+++ sensob.py
@@ -97,6 +97,7 @@ class CameraSensob(Sensob):
         img = list(pict.getdata())
         counter = 0
         for i in img:
+            print(i)
             if (i[0] <= 100) and (i[1] >= 200) and (i[2] <= 100):
                 counter += 1
         print("******Sensob, cam_value: ", counter/len(img))
